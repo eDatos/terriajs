6083296ef2394f8640e90a66c2b565c476e884ef
F. Javier Rodríguez García <frodgar@arte-consultores.com>
2025-08-20 14:46:44 +0100

    EDATOS-3870 - Actualizar a la última versión de Terria
    > Eliminada propiedad sin uso

--- lib/ReactViews/Mobile/MobileMenu.jsx
+++ lib/ReactViews/Mobile/MobileMenu.jsx
@@ -25,7 +25,6 @@ class MobileMenu extends Component {
     showFeedback: PropTypes.bool,
     terria: PropTypes.object.isRequired,
     i18n: PropTypes.object,
-    allBaseMaps: PropTypes.array.isRequired,
     t: PropTypes.func.isRequired
   };
 
